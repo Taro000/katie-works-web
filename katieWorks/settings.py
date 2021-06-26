7b31cc19d7350fdc019acb0ffb0207871bd5f67d
Shintaro Takahashi <shinta8105@gmail.com>
2021-06-26 16:00:35 +0900

    Add .gitignore

--- katieWorks/settings.py
+++ katieWorks/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--1wo8qi5(@&8wvf_@9cu^avn)3&nb_xi(-$99a(@zwj#)va4aq'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -34,6 +31,7 @@ except ImportError:
 if not DEBUG:
     import django_heroku
     django_heroku.settings(locals())
+    SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = ['*']
 
