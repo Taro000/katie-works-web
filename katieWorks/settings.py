a81a58db4213945672af2537f00b279c33256c17
Shintaro Takahashi <shinta8105@gmail.com>
2021-08-21 18:01:05 +0900

    Add AWS S3 settings

--- katieWorks/settings.py
+++ katieWorks/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'website',
     'stdimage',
     'django_cleanup.apps.CleanupConfig',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -151,3 +152,15 @@ if not DEBUG:
 
     db_from_env = dj_database_url.config()
     DATABASES['default'].update(db_from_env)
+
+    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+    MEDIA_URL = S3_URL
+
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = None
+
